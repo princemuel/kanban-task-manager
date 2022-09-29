43f45db889556c485fe0443b32cfc93ef0798798
princemuel <vansomecsam@gmail.com>
2022-09-29 17:59:43 +0100

    added redux and resolved issue with eslint

---
+++ client/common/images.ts
@@ -0,0 +1,31 @@
+import IconAddTask from 'assets/icon-add-task-mobile.svg';
+import IconBoard from 'assets/icon-board.svg';
+import IconCheck from 'assets/icon-check.svg';
+import IconChevronDown from 'assets/icon-chevron-down.svg';
+import IconChevronUp from 'assets/icon-chevron-up.svg';
+import IconCross from 'assets/icon-cross.svg';
+import IconThemeDark from 'assets/icon-dark-theme.svg';
+import IconSidebarHide from 'assets/icon-hide-sidebar.svg';
+import IconThemeLight from 'assets/icon-light-theme.svg';
+import IconSidebarShow from 'assets/icon-show-sidebar.svg';
+import IconEllipsisVertical from 'assets/icon-vertical-ellipsis.svg';
+import LogoDark from 'assets/logo-dark.svg';
+import LogoLight from 'assets/logo-light.svg';
+import LogoMobile from 'assets/logo-mobile.svg';
+
+export {
+  IconAddTask,
+  IconBoard,
+  IconCheck,
+  IconChevronUp,
+  IconChevronDown,
+  IconCross,
+  IconThemeLight,
+  IconThemeDark,
+  LogoLight,
+  LogoDark,
+  LogoMobile,
+  IconSidebarShow,
+  IconSidebarHide,
+  IconEllipsisVertical,
+};
