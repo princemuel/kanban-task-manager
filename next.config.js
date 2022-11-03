685fa843a844d8b2e9b5d9d11d8d71bddb3b193b
princemuel <vansomecsam@gmail.com>
2022-11-03 16:38:53 +0100

    Updated next config to use svgs

--- next.config.js
+++ next.config.js
@@ -3,8 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
-    styledComponents: true,
-    removeConsole: true,
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
   },
 };
 
