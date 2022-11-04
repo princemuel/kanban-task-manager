f43fd4ce660d1c9af34c5738e74d100ddf4b3ffb
princemuel <vansomecsam@gmail.com>
2022-11-04 02:42:10 +0100

    Setup data nmodels

--- types/models.ts
+++ types/models.ts
@@ -6,20 +6,24 @@ export interface IData {
   boards: IBoard[];
 }
 export interface IBoard {
+  id: string;
   name: string;
   columns: IColumn[];
 }
 export interface IColumn {
+  id: string;
   name: string;
   tasks: ITask[];
 }
 export interface ITask {
+  id: string;
   title: string;
   description: string;
   status: TaskStatus;
   subtasks: ISubtask[];
 }
 export interface ISubtask {
+  id: string;
   title: string;
   isCompleted: boolean;
 }
