2ab051a2e98edbab61bfbc0bf3961cbcb4146482
princemuel <vansomecsam@gmail.com>
2022-11-04 03:13:17 +0100

    Created column model

--- lib/schema/columns.ts
+++ lib/schema/columns.ts
@@ -1,1 +1,20 @@
-export {};
+import { Field, ID, ObjectType } from 'type-graphql';
+import type { ITask } from 'types';
+import { Task } from './tasks';
+
+export interface IColumn {
+  id: string;
+  name: string;
+  tasks: ITask[];
+}
+@ObjectType()
+export class Column {
+  @Field(() => ID)
+  id!: string;
+
+  @Field(() => String)
+  name!: string;
+
+  @Field(() => [Task])
+  tasks?: Task[];
+}
