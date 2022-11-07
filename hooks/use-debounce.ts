31cae1917ba8bcf22518f8492676c403a3592775
princemuel <vansomecsam@gmail.com>
2022-11-07 09:51:14 +0100

    Setup

---
+++ hooks/use-debounce.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+/** This hook
+ *
+ * @param value T
+ * @param delay number
+ * @returns T
+ */
+export const useDebounce = <T>(value: T, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
--- utils/clean.ts
+++
@@ -1,3 +0,0 @@
-export const clean = (data: string) => {
-  return data?.replace(/[|&;$%@"<>()\\+,]/g, '');
-};
