0b8ac65d3f7fa910a9ddf9b51c622b8392be86e8
Prince Muel <vansomecsam@gmail.com>
2022-12-31 03:43:10 +0100

    chore: cleanup and refactor modal forms
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/organisms/modal/view-board.tsx
+++ components/organisms/modal/view-board.tsx
@@ -1,35 +1,24 @@
-import { useModalStore } from 'context';
-import { BaseModal } from './base-modal';
+import { useModalDispatch } from 'context';
 
 type Props = {};
 
 export const ViewBoardModal = (props: Props) => {
-  const [isActive, setIsActive] = useModalStore(
-    (state) => state['isModalOpen']
-  );
+  const dispatch = useModalDispatch();
 
   return (
-    <BaseModal
-      open={isActive}
-      handleCloseModal={() => setIsActive({ isModalOpen: false })}
-    >
-      <form className='form' method='dialog'>
-        <header>
-          <h1>VIEW BOARD MODAL</h1>
-          <button
-            type='button'
-            onClick={() => setIsActive({ isModalOpen: false })}
-          >
-            Close
-          </button>
-        </header>
-        <label htmlFor='name'>Your name</label>
-        <input type='text' name='name' id='name' />
-        <label htmlFor='email'>Your email</label>
-        <input type='email' name='email' id='email' />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-        <button type='submit'>Submit</button>
-      </form>
-    </BaseModal>
+    <form className='form' method='dialog'>
+      <header>
+        <h1>VIEW BOARD MODAL</h1>
+        <button type='button' onClick={() => dispatch({ type: 'CLOSE_MODAL' })}>
+          Close
+        </button>
+      </header>
+      <label htmlFor='name'>Your name</label>
+      <input type='text' name='name' id='name' />
+      <label htmlFor='email'>Your email</label>
+      <input type='email' name='email' id='email' />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      <button type='submit'>Submit</button>
+    </form>
   );
 };
