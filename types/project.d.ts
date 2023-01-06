255a5885ebb926bc97d17fc745fcc108ae397964
Prince Muel <vansomecsam@gmail.com>
2023-01-06 04:56:35 +0100

    ci: format tsconfig path
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- types/project.d.ts
+++ types/project.d.ts
@@ -5,7 +5,9 @@ export declare module "react" {
   ): (props: P & React.RefAttributes<T>) => React.ReactElement | null;
 }
 
-declare type $ElementProps<T> = T extends React.ComponentType<infer Props>
+export declare type $ElementProps<T> = T extends React.ComponentType<
+  infer Props
+>
   ? Props extends object
     ? Props
     : never
