242539e2864e4fd286286dae2a5abfc2d946554a
Prince Muel <vansomecsam@gmail.com>
2023-01-06 03:24:26 +0100

    feat(ui): scaffold add board form component
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/organisms/add-board.tsx
+++ components/organisms/add-board.tsx
@@ -1,24 +1,75 @@
-import { useModalDispatch } from 'context';
+import { Dialog } from "@headlessui/react";
+import { icons } from "common";
+import { FormInput, FormLabel } from "components/atoms";
+import { useModalDispatch, useModalState } from "context";
+import { useRef } from "react";
+import { BaseModal } from "./base-modal";
 
 type Props = {};
 
-export const AddBoardModal = (props: Props) => {
-  const dispatch = useModalDispatch();
+export const AddBoard = (props: Props) => {
+  const modalState = useModalState();
+  const modalDispatch = useModalDispatch();
+  let inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className='form' method='dialog'>
-      <header>
-        <h1>ADD BOARD MODAL</h1>
-        <button type='button' onClick={() => dispatch({ type: 'CLOSE_MODAL' })}>
-          Close
+    <BaseModal
+      focusRef={inputRef}
+      isOpen={modalState.open}
+      closeModal={() => modalDispatch({ type: "CLOSE_MODAL" })}
+    >
+      <Dialog.Title as='h2' className={"font-bold"}>
+        Add New Board
+      </Dialog.Title>
+
+      <div className='> * + * space-y-4'>
+        <FormLabel htmlFor='name'>Name</FormLabel>
+        <FormInput
+          type='text'
+          ref={inputRef}
+          id='name'
+          placeholder='e.g. Web Design'
+        />
+      </div>
+
+      <fieldset className='> * + * space-y-4'>
+        <FormLabel variant='legend'>Columns</FormLabel>
+
+        <div className='grid grid-cols-[1fr,auto] items-center gap-8'>
+          <FormInput type='text' name={`column`} placeholder='Todo' />
+          <button
+            type='button'
+            className='inline-flex items-center justify-end text-brand-400 hover:text-accent-100 active:text-accent-100'
+          >
+            <icons.board.cross className='fill-current ' />
+            <span className='sr-only'>Remove</span>
+          </button>
+        </div>
+        <div className='grid grid-cols-[1fr,auto] items-center gap-8'>
+          <FormInput type='text' name={`column`} placeholder='Doing' />
+          <button
+            type='button'
+            className='inline-flex items-center justify-end text-brand-400 hover:text-accent-100 active:text-accent-100'
+          >
+            <icons.board.cross className='fill-current ' />
+            <span className='sr-only'>Remove</span>
+          </button>
+        </div>
+
+        <button
+          type='button'
+          className='inline-grid w-full place-items-center rounded-pill bg-brand-500/10 py-4 text-400 font-bold leading-400 text-brand-500 dark:bg-neutral-100'
+        >
+          + Add New Column
         </button>
-      </header>
-      <label htmlFor='name'>Your name</label>
-      <input type='text' name='name' id='name' />
-      <label htmlFor='email'>Your email</label>
-      <input type='email' name='email' id='email' />
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-      <button type='submit'>Submit</button>
-    </form>
+      </fieldset>
+
+      <button
+        type='button'
+        className='inline-grid w-full place-items-center rounded-pill bg-brand-500 py-4 text-400 font-bold leading-400 text-neutral-100'
+      >
+        Create New Board
+      </button>
+    </BaseModal>
   );
 };
