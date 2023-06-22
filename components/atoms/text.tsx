d9edb02f7dceb84fa31c6824795a545f69600d7a
Prince Muel <vansomecsam@gmail.com>
2023-06-22 02:02:18 +0100

    feat: scaffold text component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/atoms/text.tsx
+++ components/atoms/text.tsx
@@ -1,20 +1,66 @@
-import * as React from "react";
+import { cn } from '@/lib';
+import type { VariantProps } from 'cva';
+import { cva } from 'cva';
 
-type TextProps<E extends React.ElementType<any>> = {
-  children: React.ReactNode;
-  variant?: E;
-};
+const textVariants = cva('', {
+  defaultVariants: {
+    intent: 'primary',
+    size: 's',
+  },
+  variants: {
+    intent: {
+      primary: 'text-brand-900 dark:text-white',
+      secondary: 'text-brand-400 dark:text-brand-400',
+      outline: '',
+    },
+    size: {
+      s: '',
+      xs: 'text-300 leading-200',
+      sm: 'text-400 leading-400',
+      md: 'text-500 leading-300',
+      lg: 'text-600 leading-400',
+      xl: 'text-700 leading-500',
+    },
 
-type Props<E extends React.ElementType<any>> = TextProps<E> &
-  Omit<React.ComponentPropsWithoutRef<E>, keyof TextProps<E>>;
+    weight: {
+      bold: 'font-bold',
+      medium: 'font-medium',
+      regular: 'font-normal',
+    },
+  },
+  compoundVariants: [
+    {
+      intent: 'secondary',
+      size: 'xs',
+      weight: 'bold',
+      class: 'tracking-100',
+    },
+  ],
+});
 
-const Text = <E extends React.ElementType = "p">({
+interface TextVariants extends VariantProps<typeof textVariants> {}
+const text = (variants: TextVariants, className = '') =>
+  cn(textVariants(variants), className);
+// const text = (variants: TextVariants) => textVariants(variants);
+
+type Props<E extends React.ElementType = 'p'> = ElementProps<E> & TextVariants;
+
+const Text = <E extends React.ElementType = 'p'>({
+  as,
+  intent,
+  weight,
+  size,
+  className,
   children,
-  variant,
   ...rest
 }: Props<E>) => {
-  const RenderedElement = variant || "p";
-  return <RenderedElement {...rest}>{children}</RenderedElement>;
+  const Rendered = as || 'p';
+
+  return (
+    <Rendered className={text({ intent, weight, size }, className)} {...rest}>
+      {children}
+    </Rendered>
+  );
 };
 
 export { Text };
