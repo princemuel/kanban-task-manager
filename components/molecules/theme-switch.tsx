d93818b8413670e3661bb8babf2397710b55f8c4
Prince Muel <vansomecsam@gmail.com>
2023-06-22 02:52:24 +0100

    feat: extract theme switch component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/molecules/theme-switch.tsx
@@ -0,0 +1,24 @@
+import { icons } from '@/common';
+import { cn } from '@/lib';
+import { Switch } from '../atoms';
+
+interface Props {
+  className?: string;
+}
+
+const ThemeSwitch = ({ className }: Props) => {
+  return (
+    <div
+      className={cn(
+        'flex items-center justify-center gap-10 rounded-lg bg-brand-100 px-12 py-4 dark:bg-brand-800',
+        className
+      )}
+    >
+      <icons.mode.sun xlinkTitle='Home Logo' />
+      <Switch />
+      <icons.mode.moon />
+    </div>
+  );
+};
+
+export { ThemeSwitch };
