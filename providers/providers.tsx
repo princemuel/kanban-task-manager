2ad127efc69b17abe90b66e398a2469515f8c593
Prince Muel <vansomecsam@gmail.com>
2023-06-22 02:11:18 +0100

    feat: add root provider component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ providers/providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: Props) {
+  return (
+    <ThemeProvider
+      storageKey='ktm-theme'
+      defaultTheme='system'
+      enableSystem={true}
+      attribute='data-color'
+      // disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
