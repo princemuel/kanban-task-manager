53266f519f2b4a4f19156d2b19f7168fc3aa79ca
Prince Muel <vansomecsam@gmail.com>
2023-06-22 02:09:27 +0100

    chore: cleanup tailwind config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,73 +10,54 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    borderRadius: {
-      ...defaultTheme.borderRadius,
-      pill: '100vmax',
-    },
-    colors: {
-      current: 'currentColor',
-      transparent: 'transparent',
-      inherit: 'inherit',
-      neutral: { 100: '#ffffff', 900: '#000000' },
-      brand: {
-        100: '#f4f7fd',
-        200: '#e4ebfa',
-        300: '#a8a4ff',
-        400: '#828fa3',
-        500: '#635fc7',
-        600: '#3e3f4e',
-        700: '#2b2c37',
-        800: '#20212c',
-        900: '#000112',
-      },
-      accent: {
-        100: '#ff9898',
-        200: '#ea5555',
-        300: '#49C4E5',
-        400: '#8471F2',
-        500: '#67E2AE',
-      },
-    },
-    fontSize: {
-      100: '0.8rem',
-      200: '1rem',
-      300: '1.2rem',
-      400: '1.3rem',
-      500: '1.5rem',
-      600: '1.8rem',
-      700: '2.4rem', //
-    },
-    lineHeight: {
-      100: '0.8rem',
-      200: '1.5rem',
-      300: '1.9rem',
-      400: '2.3rem',
-      500: '3rem',
-    },
-    letterSpacing: {
-      100: '2.4px',
-      ...defaultTheme.letterSpacing,
-    },
-
-    // screens: {
-    //   s: "20em", // => @media (min-width: 320px) { ... }
-    //   xs: "30em", // => @media (min-width: 480px) { ... }
-    //   sm: "36em", // => @media (min-width: 576px) { ... }
-    //   sx: "40em", // => @media (min-width: 640px) { ... }
-    //   md: "48em", // => @media (min-width: 768px) { ... }
-    //   lg: "64em", // => @media (min-width: 1024px) { ... }
-    //   xl: "80em", // => @media (min-width: 1280px) { ... }
-    //   xxl: "96em", // => @media (min-width: 1536px) { ... }
-    //   xxxl: "112.5em", // => @media (min-width: 1800px) { ... }
-    // },
-
     screens: {
       sx: '20em', // => @media (min-width: 320px) { ... }
       s: '30em', // => @media (min-width: 480px) { ... }
       ...defaultTheme.screens,
     },
     extend: {
+      colors: {
+        brand: {
+          100: '#f4f7fd',
+          200: '#e4ebfa', //lines-light
+          300: '#a8a4ff',
+          400: '#828fa3',
+          500: '#635fc7',
+          600: '#3e3f4e', //lines-dark
+          700: '#2b2c37',
+          800: '#20212c',
+          900: '#000112',
+        },
+        accent: {
+          100: '#ff9898',
+          200: '#ea5555',
+          300: '#49C4E5',
+          400: '#8471F2',
+          500: '#67E2AE',
+        },
+      },
+      borderRadius: {
+        pill: '100vmax',
+      },
+      fontSize: {
+        100: '0.8rem',
+        200: '1rem',
+        300: '1.2rem',
+        400: '1.3rem',
+        500: '1.5rem',
+        600: '1.8rem',
+        700: '2.4rem', //
+      },
+      lineHeight: {
+        100: '0.8rem',
+        200: '1.5rem',
+        300: '1.9rem',
+        400: '2.3rem',
+        500: '3rem',
+      },
+      letterSpacing: {
+        100: '2.4px',
+      },
       cursor: {
         pointer:
           'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAUCAYAAABvVQZ0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAI6SURBVHgBnZRBaxNBGIYnu9uNSW1NlkgMVAl4Cyg5CAUPJlYFTyaX3CohePKUn5BcBA9CfkAvAf+B4sVLBC8eChv05ClKVaQIiUKjabOO77s7E8bUBpsXnrwzu9lvvvm+2bXFccWADRw1XlpWJpNZg+8ACV4kEomNVqtliSW0Am2mUik5HA5luVxmwEe5XC4pltBZ27a38/m8pHq9HoP54KI47ZbT6fQ52H1mRjE7jnFtK5vNrjJzhSuiui5cYA2Z3YN/HwwGYcBischgH8En8BLsKZ5w8VqtdiwoJ5bneeuO49zGeBdFD4OVSqVwu77vh1nS9VgF3SoUCq4ZiJObzAgcgK8sPtVsNqWWvka1221d011wgckI9cPJ5263K7k9NkDXbZH4Xzz3DRRVLcPDycmYBTe6KPV8kUR0Hm+AM0JFvA4+6DpRnU5nNq5UKn85A5jBUOdb8ITO7CrY4dZ0F+dX/5cbmd0Fq6zXb7CPI/F6NBq9bTQa4n/V7/dpI2QmZxfZWugKhm3eNLdI6S5q1/VkozB+BjZB3Dwe6+COZVlP4Ufs7CLV63UGeg8egksieiNmspPJZA6+DZ6DX/MZVqvVMDt1JA6w8GP4NV38ebnxePwy/IEK+FMVWHMIhmr8BnWuwj2hDqwzF+xwMpnsIeCr6XQ6DoJgIKIvhhuLxaZIjm/IEZgAH/ffwX+IqIknvvU8e+fBBlb38JALDxBwrLKTWGwf/gWMzcKfJP3pXjGuBcqlyjAwH/gDZjDKatJ5fJYAAAAASUVORK5CYII="), pointer;',
@@ -91,7 +72,7 @@ module.exports = {
         '3xl': '112.5em', // => @media (min-width: 1800px) { ... }
       },
       fontFamily: {
-        sans: ['var(--font-sans)'],
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
         100: '0px 4px 6px rgba(54, 78, 126, 0.101545)',
