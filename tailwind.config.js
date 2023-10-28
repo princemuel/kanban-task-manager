39af628b8379f84e66245dd02e2d29d3b6a4e759
Prince Muel <vansomecsam@gmail.com>
2023-10-28 06:05:30 +0100

    ci: update tailwind, vscode, typescript config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
   ],
   theme: {
     screens: {
-      s: '30em', // @media (min-width: 480px) { ... }
+      '3xs': '24em', // @media (min-width: 384px) { ... }
+      '2xs': '30em', // @media (min-width: 480px) { ... }
       ...defaultTheme.screens,
     },
     extend: {
@@ -42,20 +43,20 @@ module.exports = {
         sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
-        100: '0.8rem',
-        200: '1rem',
-        300: '1.2rem',
-        400: '1.3rem',
-        500: '1.5rem',
-        600: '1.8rem',
-        700: '2.4rem', //
+        100: '0.5rem',
+        200: '0.625rem',
+        300: '0.75rem',
+        400: '0.8125rem',
+        500: '0.9375rem',
+        600: '1.125rem',
+        700: '1.5rem', //
       },
       lineHeight: {
-        100: '0.8rem',
-        200: '1.5rem',
-        300: '1.9rem',
-        400: '2.3rem',
-        500: '3rem',
+        100: '0.5rem',
+        200: '0.9375rem',
+        300: '1.1875rem',
+        400: '1.4375rem',
+        500: '1.875rem',
       },
       letterSpacing: {
         100: '2.4px',
@@ -69,6 +70,7 @@ module.exports = {
         '2xl': '96em', // @media (min-width: 1536px) { ... }
         '3xl': '112.5em', // @media (min-width: 1800px) { ... }
       },
+
       cursor: {
         pointer:
           'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAUCAYAAABvVQZ0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAI6SURBVHgBnZRBaxNBGIYnu9uNSW1NlkgMVAl4Cyg5CAUPJlYFTyaX3CohePKUn5BcBA9CfkAvAf+B4sVLBC8eChv05ClKVaQIiUKjabOO77s7E8bUBpsXnrwzu9lvvvm+2bXFccWADRw1XlpWJpNZg+8ACV4kEomNVqtliSW0Am2mUik5HA5luVxmwEe5XC4pltBZ27a38/m8pHq9HoP54KI47ZbT6fQ52H1mRjE7jnFtK5vNrjJzhSuiui5cYA2Z3YN/HwwGYcBischgH8En8BLsKZ5w8VqtdiwoJ5bneeuO49zGeBdFD4OVSqVwu77vh1nS9VgF3SoUCq4ZiJObzAgcgK8sPtVsNqWWvka1221d011wgckI9cPJ5263K7k9NkDXbZH4Xzz3DRRVLcPDycmYBTe6KPV8kUR0Hm+AM0JFvA4+6DpRnU5nNq5UKn85A5jBUOdb8ITO7CrY4dZ0F+dX/5cbmd0Fq6zXb7CPI/F6NBq9bTQa4n/V7/dpI2QmZxfZWugKhm3eNLdI6S5q1/VkozB+BjZB3Dwe6+COZVlP4Ufs7CLV63UGeg8egksieiNmspPJZA6+DZ6DX/MZVqvVMDt1JA6w8GP4NV38ebnxePwy/IEK+FMVWHMIhmr8BnWuwj2hDqwzF+xwMpnsIeCr6XQ6DoJgIKIvhhuLxaZIjm/IEZgAH/ffwX+IqIknvvU8e+fBBlb38JALDxBwrLKTWGwf/gWMzcKfJP3pXjGuBcqlyjAwH/gDZjDKatJ5fJYAAAAASUVORK5CYII="), pointer;',
@@ -78,13 +80,14 @@ module.exports = {
   plugins: [
     require('tailwind-scrollbar')({ nocompatible: true }),
     require('@headlessui/tailwindcss')({ prefix: 'hui' }),
+    require('tailwindcss-animate'),
 
     plugin(function ({ addUtilities }) {
       addUtilities({
-        '.grid-auto-fit': {
-          '--min-col-size': '15rem',
+        '.grid-cols-auto': {
+          '--min-column-size': '15rem',
           gridTemplateColumns:
-            'repeat(auto-fit, minmax(min(var(--min-col-size), 100%), 1fr))',
+            'repeat(auto-fit, minmax(min(var(--min-column-size), 100%), 1fr))',
         },
       });
     }),
