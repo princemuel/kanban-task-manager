82bc02ef5ffc5538c3af5cb7645b80da4d4276a1
Prince Muel <vansomecsam@gmail.com>
2023-11-03 05:43:53 +0100

    ci: update configs
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next.config.js
+++ next.config.js
@@ -4,10 +4,33 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
     reactRemoveProperties: process.env.NODE_ENV === 'production',
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   productionBrowserSourceMaps: true,
   experimental: {
     typedRoutes: true,
     webVitalsAttribution: ['CLS', 'LCP'],
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+    ],
   },
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
