f94ca7f9df2dc2989778f74ac0d4fbf43f5982c0
Prince Muel <vansomecsam@gmail.com>
2023-11-15 23:23:39 +0100

    fix: resolve build errors (phew! finally)
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/app/error.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import NextLink from 'next/link';
+import { useEffect } from 'react';
+
+interface Props {
+  error: Error & { digest?: string };
+  reset: () => void;
+}
+
+export default function ErrorPage({ error, reset }: Props) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <section
+      aria-label='Error Message'
+      className='flex min-h-screen w-full items-center justify-center font-sans'
+    >
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-3xl font-bold'>Oops!</h1>
+        <p className='text-xl font-medium'>An error has occured...</p>
+        <p className='text-sm font-normal'>Error: {error?.message}</p>
+
+        <div className='flex items-center gap-8'>
+          <button type='button' onClick={reset} className='text-sm font-normal'>
+            Try again
+          </button>{' '}
+          <span>Or</span>
+          <NextLink href='/' className='text-sm font-normal underline'>
+            Go to the Homepage
+          </NextLink>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { HttpError } from 'http-errors-enhanced';
 import NextLink from 'next/link';
 import { useEffect } from 'react';
 
 interface Props {
-  error: Error;
+  error: Error & { digest?: string };
   reset: () => void;
 }
 
@@ -15,32 +14,29 @@ export default function ErrorPage({ error, reset }: Props) {
   }, [error]);
 
   return (
-    <section
-      aria-label='Error Message'
-      className='flex min-h-screen w-full items-center justify-center font-sans'
-    >
-      <div className='flex flex-col gap-4'>
-        <h1 className='text-3xl font-bold'>Oops!</h1>
-        <p className='text-xl font-medium'>An error has occured...</p>
-        <p className='text-sm font-normal'>
-          <span>Error:</span>{' '}
-          <span>{error instanceof HttpError && error.statusCode}</span>{' '}
-          <span>
-            {(error instanceof HttpError || error instanceof Error) &&
-              error.message}
-          </span>
-        </p>
+    <html>
+      <body>
+        <section
+          aria-label='Error Message'
+          className='flex min-h-screen w-full items-center justify-center font-sans'
+        >
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-3xl font-bold'>Oops!</h1>
+            <p className='text-xl font-medium'>An error has occured...</p>
+            <p className='text-sm font-normal'>Error: {error?.message}</p>
 
-        <div className='flex items-center gap-8'>
-          <button type='button' onClick={reset} className='text-sm font-normal'>
-            Try again
-          </button>{' '}
-          <span>Or</span>
-          <NextLink href='/' className='text-sm font-normal underline'>
-            Go to the Homepage
-          </NextLink>
-        </div>
-      </div>
-    </section>
+            <div className='flex items-center gap-8'>
+              <button onClick={reset} className='text-sm font-normal'>
+                Try again
+              </button>
+              <span>&nbsp;Or&nbsp;</span>
+              <NextLink href='/' className='text-sm font-normal underline'>
+                Go back to the Homepage
+              </NextLink>
+            </div>
+          </div>
+        </section>
+      </body>
+    </html>
   );
 }
