182bac2ec2a13dbb5e7bf3374d24d2842d63831d
Prince Muel <vansomecsam@gmail.com>
2023-11-20 13:31:04 +0100

    chore: deprecate unnecessary files
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
 export * from './form';
 export * from './helpers';
+// export * from './index.client';
+// export * from './index.server';
 export * from './layout';
+export * from './modals';
 export * from './shared';
 export * from './ui';
