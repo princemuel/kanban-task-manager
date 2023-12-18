957745362b704cb391ae71a908ea6e7ce2fd89d3
Prince Muel <vansomecsam@gmail.com>
2023-12-18 19:16:47 +0100

    feat: setup light/dark mode
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
+import { ClerkApp, ClerkErrorBoundary } from "@clerk/remix";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,27 +9,76 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { Analytics } from "@vercel/analytics/react";
+import {
+  PreventFlashOnWrongTheme,
+  ThemeProvider as RemixThemesProvider,
+  useTheme,
+} from "remix-themes";
+import { BreakpointIndicator } from "./components/breakpoint-indicator";
+import styles from "./globals.css";
+import { themeSessionResolver } from "./sessions.server";
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export default function App() {
+export const loader = (args: LoaderFunctionArgs) => {
+  return rootAuthLoader(args, async ({ request }) => {
+    const { getTheme } = await themeSessionResolver(request);
+
+    return { theme: getTheme() };
+  });
+};
+
+export const ErrorBoundary = ClerkErrorBoundary();
+
+function App() {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
+
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      dir="ltr"
+      data-darkreader-mode="dynamic"
+      data-darkreader-theme={theme ?? ""}
+      // className={tw`__sans__`}
+    >
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
-      <body>
+
+      <body className="relative min-h-screen antialiased">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <Analytics />
+
+        <BreakpointIndicator />
       </body>
     </html>
   );
 }
+
+function Root() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <RemixThemesProvider
+      specifiedTheme={data.theme}
+      themeAction="/action/set-theme"
+    >
+      <App />
+    </RemixThemesProvider>
+  );
+}
+
+export default ClerkApp(Root);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,56 @@
-import type { Config } from 'tailwindcss'
+import { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
 
 export default {
-  content: [],
-  theme: {
-    extend: {},
+  darkMode: ["class", '[data-darkreader-theme="dark"]'],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  corePlugins: {
+    float: false,
+    container: false,
   },
-  plugins: [],
-} satisfies Config
-
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    screens: {
+      "3xs": "24em", // @media (min-width: 384px) { ... }
+      "2xs": "30em", // @media (min-width: 480px) { ... }
+      ...defaultTheme.screens,
+    },
+    extend: {
+      borderRadius: {
+        pill: "100vmax",
+      },
+      screens: {
+        xs: "36em", // @media (min-width: 576px) { ... },
+        sm: "40em", // @media (min-width: 640px) { ... }
+        md: "48em", // @media (min-width: 768px) { ... }
+        lg: "64em", // @media (min-width: 1024px) { ... }
+        xl: "80em", // @media (min-width: 1280px) { ... }
+        "2xl": "96em", // @media (min-width: 1536px) { ... }
+        "3xl": "112.5em", // @media (min-width: 1800px) { ... }
+      },
+      animation: {
+        float: "float 6s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translate3d(0px, -8px, 0)" },
+          "50%": { transform: "translate3d(0px, 8px, 0)" },
+        },
+      },
+      cursor: {
+        pointer:
+          'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAUCAYAAABvVQZ0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAI6SURBVHgBnZRBaxNBGIYnu9uNSW1NlkgMVAl4Cyg5CAUPJlYFTyaX3CohePKUn5BcBA9CfkAvAf+B4sVLBC8eChv05ClKVaQIiUKjabOO77s7E8bUBpsXnrwzu9lvvvm+2bXFccWADRw1XlpWJpNZg+8ACV4kEomNVqtliSW0Am2mUik5HA5luVxmwEe5XC4pltBZ27a38/m8pHq9HoP54KI47ZbT6fQ52H1mRjE7jnFtK5vNrjJzhSuiui5cYA2Z3YN/HwwGYcBischgH8En8BLsKZ5w8VqtdiwoJ5bneeuO49zGeBdFD4OVSqVwu77vh1nS9VgF3SoUCq4ZiJObzAgcgK8sPtVsNqWWvka1221d011wgckI9cPJ5263K7k9NkDXbZH4Xzz3DRRVLcPDycmYBTe6KPV8kUR0Hm+AM0JFvA4+6DpRnU5nNq5UKn85A5jBUOdb8ITO7CrY4dZ0F+dX/5cbmd0Fq6zXb7CPI/F6NBq9bTQa4n/V7/dpI2QmZxfZWugKhm3eNLdI6S5q1/VkozB+BjZB3Dwe6+COZVlP4Ufs7CLV63UGeg8egksieiNmspPJZA6+DZ6DX/MZVqvVMDt1JA6w8GP4NV38ebnxePwy/IEK+FMVWHMIhmr8BnWuwj2hDqwzF+xwMpnsIeCr6XQ6DoJgIKIvhhuLxaZIjm/IEZgAH/ffwX+IqIknvvU8e+fBBlb38JALDxBwrLKTWGwf/gWMzcKfJP3pXjGuBcqlyjAwH/gDZjDKatJ5fJYAAAAASUVORK5CYII="), pointer;',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("optional", "&:optional");
+      addVariant("hocus", ["&:hover", "&:focus"]);
+      addVariant("inverted-colors", "@media (inverted-colors: inverted)");
+    }),
+  ],
+} satisfies Config;
